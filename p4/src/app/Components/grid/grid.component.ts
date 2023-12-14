d092e193c755e4e10f162370a63ecedb080858ae
ronanter <ronan1terras@gmail.com>
2023-12-14 16:48:18 +0100

    [FIX] spécial loïs

--- p4/src/app/Components/grid/grid.component.ts
+++ p4/src/app/Components/grid/grid.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
+import {HoleComponent} from "../hole/hole.component";
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [],
+  imports: [
+    HoleComponent
+  ],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css'
 })
 export class GridComponent {
 
-  private tab!: number[][];
+  tab: number[][] = [[0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0],
+                    [0,0,0,0,0,0,0]];
+
+  public get_color(nb : number){
+    let color;
+    switch(nb) {
+      case 0: {
+        color = "red"
+        break;
+      }
+      case 1: {
+        color = "red"
+        break;
+      }
+      case 2: {
+        color = "yellow"
+        break;
+      }
+    }
+    return color;
+    };
 
 }
