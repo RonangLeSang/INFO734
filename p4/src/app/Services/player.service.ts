ff2ce41ac9b8d91c6b7b91647fba51037e019b99
loisblin <blinlois@gmail.com>
2023-12-14 17:31:21 +0100

    [FEATURE] player service

--- p4/src/app/Services/player.service.ts
+++ p4/src/app/Services/player.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+// @ts-ignore
+import { Player } from './playerModel';
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
+  //recup l'api
+
+
 
   constructor() { }
+  // @ts-ignore
+  getPlayerById(id: number): Observable<Player> {
+
+    //retourne player ,id ,nom ,nombre win ,nombre game
+
+  }
 }
