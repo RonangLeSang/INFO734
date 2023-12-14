0a5908f95c4a00130f2ad932fb84569ceac98218
ronanter <ronan1terras@gmail.com>
2023-12-14 14:19:24 +0100

    [FEATURE] component Token that works a little

---
+++ FrontEnd/p4/src/app/Components/turn/turn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-turn',
+  standalone: true,
+  imports: [],
+  templateUrl: './turn.component.html',
+  styleUrl: './turn.component.css'
+})
+export class TurnComponent {
+
+}
