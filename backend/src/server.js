a302fe6357bbd0e5abdabc6ce7e6326607207388
adam <dede13210@gmail.com>
2023-12-21 17:53:37 +0100

    [FEATURE] create game + session

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { MongoClient } = require('mongodb');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 const port = 3000;
@@ -13,7 +15,13 @@ const adress = "193.48.125.44";
 const url = `mongodb://${user}:${password}@${adress}:27017/?authMechanism=DEFAULT&authSource=admin`;
 const dbName = "puissance4";
 
-app.use(bodyParser.json());
+app.use(session({
+    secret: 'pommedapi', // Change this to a strong, random key
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ url: url+"/"+dbName }) // If using MongoDB for session storage
+    }));
+    app.use(bodyParser.json());
 
 // Create a new MongoClient
 const client = new MongoClient(url);
@@ -28,19 +36,26 @@ try {
 
     // Define User collection
     const userCollection = db.collection('users');
+    const gamesCollection = db.collection('games');
 
     // Register endpoint
     app.post('/register', async (req, res) => {
         try {
             const { username, password } = req.body;
+            if (await userCollection.findOne({ username })){
+                res.status(400).json({ message: 'User already exist' });
+                return;
+            }
 
             // Hash the password before saving it to the database
             const hashedPassword = await bcrypt.hash(password, 10);
 
             // Insert user into the collection
             await userCollection.insertOne({
-                username,
+                username ,
                 password: hashedPassword,
+                gameCount: 0,
+                victoryCount:0,
             });
 
             res.json({ message: 'User registered successfully' });
@@ -68,6 +83,8 @@ try {
                 return res.status(401).json({ message: 'Invalid username or password' });
             }
 
+
+
             // Create a JWT token for authentication
             const token = jwt.sign({ username: user.username }, 'your-secret-key');
 
@@ -76,7 +93,39 @@ try {
             console.error('Error during login:', error);
             res.status(500).json({ error: 'Internal Server Error' });
         }
-    });}catch (e){
+    });
+    app.post('/createGame',async (req,res) =>{
+        try{
+            const { idPlayer } = req.body;
+            // Create a new document with the specified fields
+            const newGame = {
+            id1: idPlayer,
+            id2: 'none',
+            gray : [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]],
+            nextplayer: 'id1',
+            };
+
+            // Insert the new document into the "game" collection
+            const result = await gamesCollection.insertOne(newGame);
+        res.json({ message: 'game successfully creat' });
+    } catch (error) {
+        console.error('Error during registration:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    });
+    app.post('/logout', (req, res) => {
+        // Destroy the session
+        req.session.destroy(err => {
+            if (err) {
+                console.error('Error destroying session:', err);
+                return res.send('Logout failed');
+            }
+
+            res.send('Logout successful');
+        });
+    });
+}catch (e){
     console.error(e);
 }
 
