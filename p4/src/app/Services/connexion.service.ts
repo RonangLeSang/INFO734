5bfc8a87e7aa788a9b4f33d294cb83e779441fb9
loisblin <blinlois@gmail.com>
2024-01-09 17:06:32 +0100

    [Feature]page de connection

---
+++ p4/src/app/Services/connexion.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import {Player} from "../playerModel";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConnexionService {
+  private endpoints='server.js';
+
+  constructor(private http: HttpClient) { }
+  registerPlayer(player: Player): Observable<Player> {
+    return this.http.post<Player>(`${this.endpoints}/register`, player);
+
+
+
+
+}
+
+}
