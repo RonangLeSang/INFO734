964fa0a8c11d48b2b0e8afaaf63021c7d54e7346
ronanter <ronan1terras@gmail.com>
2024-01-10 12:01:39 +0100

    [FIX] position de la grille

--- p4/src/app/Components/grid/grid.component.ts
+++ p4/src/app/Components/grid/grid.component.ts
@@ -11,11 +11,11 @@ import {GridService} from "../../Services/grid.service";
 export class GridComponent implements AfterViewInit {
 
   tab: number[][] = [[0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0],
-                    [0,0,-1,0,1,0,0],
-                    [0,1,1,1,-1,0,0]];
+    [0,0,0,0,0,0,0],
+    [0,0,0,1,0,0,0],
+    [0,0,0,-1,0,0,0],
+    [0,1,-1,-1,0,0,0],
+    [1,2,-1,-1,0,0,0]];
   @ViewChild('canvas')
   canvas!: HTMLCanvasElement;
 
