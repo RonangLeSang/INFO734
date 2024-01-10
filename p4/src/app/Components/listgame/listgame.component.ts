0bc0d723792094382a40d93e680598673f41d844
adam <dede13210@gmail.com>
2024-01-10 16:18:54 +0100

    [Feature]listgame service

---
+++ p4/src/app/Components/listgame/listgame.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listgame',
+  standalone: true,
+  imports: [],
+  templateUrl: './listgame.component.html',
+  styleUrl: './listgame.component.css'
+})
+export class ListgameComponent {
+
+}
