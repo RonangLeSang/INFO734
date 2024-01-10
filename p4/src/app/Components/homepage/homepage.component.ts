6f3b42fbbb3a61abb8a19866be3f2e15482c21eb
loisblin <blinlois@gmail.com>
2024-01-10 23:50:32 +0100

    [Feature]routage page connexion/ liste partie

--- p4/src/app/Components/homepage/homepage.component.ts
+++ p4/src/app/Components/homepage/homepage.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ConnexionService } from '../../Services/connexion.service';
 import {FormsModule} from "@angular/forms";
 import {CookieService} from "ngx-cookie-service";
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './homepage.component.html',
@@ -20,7 +20,8 @@ export class HomepageComponent implements OnInit {
 
   @Output() loginSuccess = new EventEmitter<number[][]>();
 
-  constructor(private connexionService: ConnexionService,private cookie:CookieService) {}
+
+  constructor(private connexionService: ConnexionService,private cookie:CookieService, private router: Router) {}
 
   ngOnInit(): void {
   }
@@ -29,6 +30,7 @@ export class HomepageComponent implements OnInit {
     this.connexionService.login(this.user, this.password).
     subscribe((response: string) => {
       this.cookie.set('cookie',response);
+      this.router.navigate(['/list']);
       console.log(response);
     }), (error: any) => {
         console.error('Login error:', error);
