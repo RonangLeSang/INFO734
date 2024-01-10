b8f7b9a936c085951b44c9dbc27a48987bbe6fae
loisblin <blinlois@gmail.com>
2024-01-11 00:54:13 +0100

    [Feature]register

--- p4/src/app/app.routes.ts
+++ p4/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import {GridComponent} from "./Components/grid/grid.component";
 import {HomepageComponent} from "./Components/homepage/homepage.component";
 import {ListgameComponent} from "./Components/listgame/listgame.component";
-
+import {RegisterpageComponent} from "./Components/registerpage/registerpage.component";
 
 export const routes: Routes = [
   {path:'login',component:HomepageComponent},
   {path:'grid',component:GridComponent},
-  {path:'list',component:ListgameComponent}
+  {path:'list',component:ListgameComponent},
+  {path:'register',component:RegisterpageComponent}
 
 ];
