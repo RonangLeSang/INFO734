13607b59917000280918756a73913d20afb3c9d6
loisblin <blinlois@gmail.com>
2024-01-10 17:58:38 +0100

    [Feature]homepage qui s'affiche mais sans form

--- p4/src/app/Services/connexion.service.ts
+++ p4/src/app/Services/connexion.service.ts
@@ -2,18 +2,29 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, map, Observable, throwError } from 'rxjs';
+
 import {Player} from "../playerModel";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnexionService {
-  private endpoints='http://localhost:3000/';
+  apiUrl='http://localhost:3000/';
+  currentUser = {};
 
-  constructor(private http: HttpClient) { }
 
-  loginPlayer(player:Player): Observable<Player> {
-    return this.http.post<Player>(`${this.endpoints}login`, player);
+  constructor(private http: HttpClient, public router: Router) {}
+
+
+  loginPlayer(player:Player) Observable<number[][]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    const url = `${this.apiUrl}makeAMove`;
+    const requestBody = { move: pos };
+    return this.http.post(url, requestBody, { headers }).pipe(
+      map((data: any) => data.grid)
+    );
   }
 
 }
