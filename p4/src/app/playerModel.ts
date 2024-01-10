a34def44f7b611cce1eaabee0bed6cc53d50377b
loisblin <blinlois@gmail.com>
2024-01-10 17:58:30 +0100

    [Feature]homepage qui s'affiche mais sans form

--- p4/src/app/playerModel.ts
+++ p4/src/app/playerModel.ts
@@ -1,12 +1,26 @@
 export class Player {
-  login: string;
-  mdp: string;
+  get login(): string {
+    return this._login;
+  }
+
+  set login(value: string) {
+    this._login = value;
+  }
+  get mdp(): string {
+    return this._mdp;
+  }
+
+  set mdp(value: string) {
+    this._mdp = value;
+  }
+  private _login: string;
+  private _mdp: string;
 
 
 
   constructor(login: string, mdp: string) {
-    this.login = login;
-    this.mdp = mdp;
+    this._login = login;
+    this._mdp = mdp;
 
 
   }
