8a904d1315a9bf55ebecbbeccf2e0bf7aa4bcacc
adam <dede13210@gmail.com>
2024-01-10 11:51:09 +0100

    [Feature]routing

--- p4/src/app/app.component.ts
+++ p4/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {GridComponent} from "./Components/grid/grid.component";
 import {HomepageComponent} from "./Components/homepage/homepage.component";
 
@@ -8,7 +8,7 @@ import {HomepageComponent} from "./Components/homepage/homepage.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,GridComponent],
+  imports: [CommonModule, RouterLink,RouterLinkActive,RouterOutlet,GridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
