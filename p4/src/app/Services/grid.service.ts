7f9507d1cf1edaf64f7818633d2357a6b37064e1
ronanter <ronan1terras@gmail.com>
2024-01-10 09:02:08 +0100

    [FEATURE] début de lien entre le back et le front

---
+++ p4/src/app/Services/grid.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import {map, Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GridService {
+
+  private apiUrl = 'http://localhost:3000/';
+
+  constructor(private http: HttpClient) {}
+
+  getGrid(): Observable<number[][]> {
+    const url = `${this.apiUrl}GetGrid`;
+    return this.http.get(url).pipe(
+      map((data: any) => data.grid)
+    );
+  }
+
+  makeAMove(pos: number): Observable<number[][]> {
+    const url = `${this.apiUrl}makeAMove`;
+    const requestBody = { position: pos }; // Assuming you need to send the position to the server
+    return this.http.post(url, requestBody).pipe(
+      map((data: any) => data.grid)
+    );
+  }
+}
